fb3fc5426ffe97bc751e447a2ccd0151aa786eb5
Felipe Dos Santos Conceição <felipe.conceicao@mercadolivre.com>
2021-12-27 11:19:47 -0300

    criacao do projeto

---
+++ src/main/java/br/com/meli/bootcapm/restauranteapi/RestauranteApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.meli.bootcapm.restauranteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestauranteApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/meli/bootcapm/restauranteapi/RestauranteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.bootcapm.restauranteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
